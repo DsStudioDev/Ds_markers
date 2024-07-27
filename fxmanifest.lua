641262549072e305b87ab96bf0d9f90358b5693d
Ds Studio <dsstudiodev@gmail.com>
2024-07-27 19:02:19 +0300

    Delete fxmanifest.lua

--- fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-version '1.0.0'
-author 'Ds Studio'
-description 'Makes Custom 3d Markers'
-repository 'https://github.com/citizenfx/cfx-server-data'
-
-client_script 'client/client.lua'
-
-fx_version 'cerulean'
-game 'gta5'
