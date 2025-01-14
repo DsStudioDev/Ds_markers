3d3c3f30f9b69d637bc5b277a0ace0481c501062
Kareem Ahmed <kareem.ahmedhu@gmail.com>
2025-01-14 17:52:21 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 version '1.0.0'
 author 'Ds Studio'
 description 'Makes Custom 3d Markers'
-repository 'https://github.com/citizenfx/cfx-server-data'
+repository 'https://github.com/DsStudioDev/Ds_markers'
 
 client_script 'client/client.lua'
 
